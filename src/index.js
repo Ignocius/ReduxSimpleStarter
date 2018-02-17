c743e16b4658c37595a3be6c9fc8715aa7fab3b3
HOME-Ricsi <szabo.ricsi@zip2000.hu>
2018-02-17 16:20:41 +0100

    yoputube api data flow started

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAdbNX6aIDhVGby1aMIYVAIEo7i-oqe1Ec';
+
+YTSearch({key: API_KEY, term: 'gopro'}, function(data) {
+  console.log(data);
+});
 
 const App = () => {
   return (
